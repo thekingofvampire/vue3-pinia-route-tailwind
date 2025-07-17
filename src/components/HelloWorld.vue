80bc57d7611ee3263553b53704d325466ec91f5d
EGHUA1 <egg.huang1@ingka.com>
2025-07-17 23:20:41 +0800

    commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 defineProps<{
   msg: string
 }>();
+
+const count = ref(0)
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ msg }} {{ count }}</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
